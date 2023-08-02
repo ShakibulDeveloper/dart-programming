64750576b0a088e7dcfd4d23e7e451fa392a7fea
ShakibulDeveloper <epiktheme@gmail.com>
2023-08-02 21:20:40 +0600

    assignment_one

---
+++ bin/operator.dart
@@ -0,0 +1,22 @@
+void main(){
+  /// Basic Notes
+  /*
+    There are some operators:
+    01. + (addition)
+    02. - (substraction)
+    03. / (division)
+    04. * (multiplication)
+    05. % (modulus: remainder)
+  */
+
+  int num_one = 30;
+  int num_two = 5;
+  int addtion = num_one+num_two;
+  int substration = num_one - num_two;
+  double division = num_one / num_two;
+  int remainder = num_one % num_two;
+  print(addtion);
+  print(substration);
+  print(division);
+  print(remainder);
+}
---
+++ lib/assignment_one.dart
@@ -0,0 +1,18 @@
+void main(){
+  /// calculation program
+
+  const a = 7;
+  const b = 3;
+
+  int addNumbers = a+b;
+  int subNumbers = a-b;
+  int multiNumbers = a*b;
+  double divNumbers = a / b;
+  int modNumbers = a % b;
+  
+  print('Addition: $a + $b = $addNumbers');
+  print('Subtraction: $a - $b = $subNumbers');
+  print('Multiplication: $a * $b = $multiNumbers');
+  print('Division: $a / $b = $divNumbers');
+  print('Modulus: $a % $b = $modNumbers');
+}
