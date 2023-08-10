72ef536875552e620facd8ae5176a516528aee0b
ShakibulDeveloper <epiktheme@gmail.com>
2023-08-10 21:08:08 +0600

    up5

---
+++ lib/assignment_two.dart
@@ -0,0 +1,18 @@
+void main(){
+  /// Student marks & average program
+
+  List<int> studentMarks = [85, 92, 78, 65, 88, 72];
+  int totalMark  = 0;
+  for (int studentMark in studentMarks) {
+    totalMark += studentMark;
+  };
+
+  double averageMark = totalMark / studentMarks.length;
+  print("Student's average grade: ${averageMark.toStringAsFixed(0)}");
+  print("Rounded average: ${averageMark.round()}");
+  if (averageMark.round() >= 70) {
+    print('Passed');
+  }else {
+    print('Failed');
+  }
+}
