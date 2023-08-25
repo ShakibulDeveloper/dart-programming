17c146fadfd09e13bc15a3f997158e0bd67d5aaa
ShakibulDeveloper <epiktheme@gmail.com>
2023-08-25 23:16:42 +0600

    DART

---
+++ lib/livetest/livetest_four.dart
@@ -0,0 +1,24 @@
+class Media{
+  void play(){
+    print("Playing media...");
+  }
+}
+
+class Song extends Media{
+  late String artist;
+
+  Song(this.artist);
+
+  @override
+  void play(){
+    print("Playing song by $artist...");
+  }
+}
+
+void main(){
+  Media media = Media();
+  Song atif = Song('Atif Islam');
+
+  media.play();
+  atif.play();
+}
